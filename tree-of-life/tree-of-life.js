99dc64733de2b172f56a3ae3078b312ce0b86911
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2024-05-18 20:31:54 +0200

    Add cubed tree of life

--- tree-of-life/tree-of-life.js
+++ tree-of-life/tree-of-life.js
@@ -1,7 +1,42 @@
 function setup() {
-
+    createCanvas(400, 400);
+    background(0);
 }
 
 function draw() {
+
+    stroke('magenta');
+
+
+    let h = 150
+    translate(200, 400)
+    rotate(PI)
+    buildTree(h)
+
+    noLoop();
+
+}
+
+function buildTree(height) {
+    if (height > 3) {
+        strokeWeight(height/50);
+        line(0, 0, 0, height)
+        translate(0, height)
+
+        push()
+        rotate(PI/3)
+        buildTree(height/3*2)
+        pop()
+
+        push()
+        buildTree(height/3*2)
+        pop()
+
+        push()
+        rotate(-PI/3)
+        buildTree(height/3*2)
+        pop()
+
+    }
 
 }
