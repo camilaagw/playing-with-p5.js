b0af28c1e63461589ed4b79dfe9b89ea40b2875b
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2024-06-29 21:24:13 +0200

    Add tree of life v1

--- tree-of-life/tree-of-life.js
+++ tree-of-life/tree-of-life.js
@@ -1,24 +1,31 @@
-let initHeigth = 150;
+let initHeigth = 175;
 let limitHeigth = initHeigth;
+let startColor, endColor;
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(500, 500);
     background(0);
     frameRate(1);
+    startColor = color('#6a16b1');
+    endColor = color('magenta');
 }
 
 function draw() {
     limitHeigth = limitHeigth/3*2
     if (limitHeigth < 2) noLoop()
-
-    stroke('magenta');
-    translate(200, 400)
+    translate(250, 500)
     rotate(PI)
     buildTree(initHeigth)
 }
 
 function buildTree(height) {
     if (height > limitHeigth) {
+
+        // Calculate the current color based on the height
+        let lerpAmt = map(height, initHeigth, limitHeigth, 0, 1);
+        lerpAmt = Math.pow(lerpAmt, 3) ;
+        let currentColor = lerpColor(startColor, endColor, lerpAmt);
+        stroke(currentColor);
         strokeWeight(height/50);
         line(0, 0, 0, height)
         translate(0, height)
