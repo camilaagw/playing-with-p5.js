00f6897b6b86fc736fc9d22acc278ccf7618b46d
Camila A. Gonzalez Williamson <camilaagw@gmail.com>
2025-06-02 15:24:59 +0200

    Add docstrings and comments to vector

--- basic-patterns/vectors/vectors.js
+++ basic-patterns/vectors/vectors.js
@@ -1,12 +1,21 @@
+/**
+ * Radial Vector Pattern
+ * Generates random 2D vectors and visualizes them as lines radiating from the center of the canvas.
+ */
+
 function setup() {
     createCanvas(500, 500);
     background(0);
 }
 
 function draw() {
-    translate(width/2, height/2)
+    translate(width / 2, height / 2); 
+
+    // Create a random 2D vector with a random direction and magnitude.
     let v = p5.Vector.random2D();
-    v.mult(random(10, 200)); // 50, 100
+    v.mult(random(10, 200));
+
+    // Draw vector.
     stroke(255, 20);
-    line(0, 0, v.x, v.y)
+    line(0, 0, v.x, v.y);
 }
