a94d6ba4de7f8cf36409750cbcec0158840bfeae
Camila A. Gonzalez Williamson <camilaagw@gmail.com>
2025-06-09 09:31:18 +0200

    Refactor confeti

--- generative-art/confeti/dots.js
+++ generative-art/confeti/dots.js
@@ -1,41 +1,74 @@
-let colors = [
-    "#CDB4DB",
-    "#FFC8DD",
-    "#FFAFCC",
-    "#BDE0FE",
-    "#A2D2FF"
-]
-let next_color = 0;
 
+/**
+ * Dots class to create a confetti-like effect with animated dots.
+ * Each set of dots has a cycling color, a position, and a random curvature.
+ */
 class Dots {
+
+    static nextColorIndex = 0;
+
+    static getNextCycleColor() {
+        const palette = [
+            "#CDB4DB",
+            "#FFC8DD",
+            "#FFAFCC",
+            "#BDE0FE",
+            "#A2D2FF"
+        ]
+        const color = palette[Dots.nextColorIndex];
+        Dots.nextColorIndex = (Dots.nextColorIndex + 1) % palette.length;
+        return color;
+    }
+
     constructor() {
-        this.init()
+        this.randomCenter = true;
+        this.chaoticMode = false;
+        this.circleSizeRange = [50, 200];
+        this.dotSizeRange = [2, 30];
+        this.init();
+    }
+
+    updateParams(params) {
+        this.randomCenter = params.randomCenter;
+        this.chaoticMode = params.chaoticMode;
+        this.circleSizeRange = [params.circleSize * 0.4, params.circleSize * 1.6];
+        this.dotSizeRange = [params.dotSize * 0.1, params.dotSize * 1.9];
     }
 
     init() {
-        this.r_min = random(2, 20)
-        this.r_max = random(this.r_min + 10, 30)
-        this.rotation = random(0, 2*PI)
-        this.angle = 0
-        this.angle_final = random(0.06, 3.1416)
-        this.color = colors[next_color];
-        next_color = (next_color + 1) % colors.length
-        this.x = random(width)
-        this.y = random(height)
-        this.curvature = random(50, 200)
+        this.rotationOffset = random(0, 2*PI)
+        this.currentAngle = 0
+        this.targetAngle = random(0, PI)
+
+        this.color = Dots.getNextCycleColor(); 
+
+        this.centerX = this.randomCenter? random(width) : width/2 
+        this.centerY =  this.randomCenter? random(height) : height/2 
+
+        this.circleRadius = random(this.circleSizeRange[0], this.circleSizeRange[1])
     }
 
     show() {
-        while (this.angle < this.angle_final) {
-            this.angle += 0.01
-            let r = map(this.angle, 0, this.angle_final,
-                this.r_min, this.r_max)
-            translate(this.x, this.y)
-            rotate(this.angle + this.rotation)
+        const ANGLE_INCREMENT = 0.01;
+
+        if (!this.chaoticMode) translate(this.centerX, this.centerY);
+    
+        // Draw the entire dots burst in one go
+        while (this.currentAngle < this.targetAngle) {
+
+            if (this.chaoticMode) translate(this.centerX, this.centerY);
+            rotate(this.currentAngle + this.rotationOffset)
+            
+            let dotRadius = random(this.dotSizeRange[0], this.dotSizeRange[1])
+
             stroke(this.color)
             fill(this.color)
-            ellipse( this.curvature, 0, r)
+            circle(this.circleRadius, 0, dotRadius)
+
+            this.currentAngle += ANGLE_INCREMENT
         }
-        this.init()
+
+        // Reset the state after showing the dot
+        this.init();
     }
 }
