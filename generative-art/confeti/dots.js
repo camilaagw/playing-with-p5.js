fd44e2e75b15c10b843f98fdbb1df210292678ae
Camila Andrea Gonzalez Williamson <camilaagw@gmail.com>
2025-06-15 17:18:40 +0200

    Adjust dots radious range

--- generative-art/confeti/dots.js
+++ generative-art/confeti/dots.js
@@ -32,7 +32,7 @@ class Dots {
         this.randomCenter = params.randomCenter;
         this.chaoticMode = params.chaoticMode;
         this.circleSizeRange = [params.circleSize * 0.4, params.circleSize * 1.6];
-        this.dotSizeRange = [params.dotSize * 0.1, params.dotSize * 1.9];
+        this.dotSizeRange = [params.dotSize * 0.125, params.dotSize * 1.9];
     }
 
     init() {
